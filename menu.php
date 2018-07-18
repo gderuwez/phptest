c5aaeeaa0ef9256fc2e13f61d4d931680b34d3a7
gderuwez <gderuwez@gmail.com>
2018-07-18 09:22:22 +0200

    heroku test

--- menu.php
+++ menu.php
@@ -12,22 +12,22 @@
   <header>
     <div class="w3-bar w3-blue-gray">
       <a href="./index.php" class="w3-bar-item w3-button w3-ripple">home</a>
-      <a href="./array.php" class="w3-bar-item w3-button w3-ripple w3-hide-small">array</a>
+      <!-- <a href="./array.php" class="w3-bar-item w3-button w3-ripple w3-hide-small">array</a>
       <a href="./boucle.php" class="w3-bar-item w3-button w3-ripple w3-hide-small">loop(de-loop)</a>
       <a href="./functions.php" class="w3-bar-item w3-button w3-ripple w3-hide-small">functions</a>
       <a href="./generator.php" class="w3-bar-item w3-button w3-ripple w3-hide-small">word generator</a>
       <a href="./functionGodMode.php" class="w3-bar-item w3-button w3-ripple w3-hide-small">Idqd functions</a>
-      <a href="./extra.php" class="w3-bar-item w3-button w3-ripple w3-hide-small">extra functions</a>
+      <a href="./extra.php" class="w3-bar-item w3-button w3-ripple w3-hide-small">extra functions</a> -->
       <a href="javascript:void(0)" class="w3-bar-item w3-button w3-right w3-hide-large w3-hide-medium" onclick="myFunction()">&#9776;</a>
     </div>
 
-    <div id="demo" class="w3-bar-block w3-blue-gray w3-hide w3-hide-large w3-hide-medium">
+    <!-- <div id="demo" class="w3-bar-block w3-blue-gray w3-hide w3-hide-large w3-hide-medium">
       <a href="./array.php" class="w3-bar-item w3-button w3-ripple">array</a>
       <a href="./boucle.php" class="w3-bar-item w3-button w3-ripple">loop(de-loop)</a>
       <a href="./functions.php" class="w3-bar-item w3-button w3-ripple">functions</a>
       <a href="./generator.php" class="w3-bar-item w3-button w3-ripple">word generator</a>
       <a href="./functionGodMode.php" class="w3-bar-item w3-button w3-ripple">Idqd functions</a>
       <a href="./extra.php" class="w3-bar-item w3-button w3-ripple">extra functions</a>
-    </div>
+    </div> -->
 
   </header>
